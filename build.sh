d95f831d49da68f6888c816cbf455ef8866e303f
William Jackson <william@subtlecoolness.com>
2020-07-02 20:57:45 -0500

    Do not use custom builder image

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-PSYCOPG2_TAG="$(/usr/bin/awk -F '==' '{ gsub(/\./, "_", $2) ; print $2 }' requirements.txt)"
-/usr/bin/git clone --branch "${PSYCOPG2_TAG}" --depth 1 https://github.com/psycopg/psycopg2.git
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache gcc git musl-dev postgresql-dev
+
+# find version
+PSYCOPG2_TAG="$(awk -F '==' '/psycopg2/ { gsub(/\./, "_", $2) ; print $2 }' requirements.txt)"
+
+# check out source code
+rm -rf psycopg2
+git config --global advice.detachedHead false
+git clone --branch "${PSYCOPG2_TAG}" --depth 1 https://github.com/psycopg/psycopg2.git
+
+# build
 cd psycopg2 || exit
-/usr/local/bin/python setup.py bdist_wheel
+python setup.py bdist_wheel
