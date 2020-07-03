e4503d9eb126635520fd4922363e1a94fc0236b8
William Jackson <william@subtlecoolness.com>
2020-07-03 10:51:03 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -3,16 +3,7 @@
 set -e
 
 # install os dependencies
-apk add --no-cache gcc git musl-dev postgresql-dev
-
-# find version
-PSYCOPG2_TAG="$(awk -F '==' '/psycopg2/ { gsub(/\./, "_", $2) ; print $2 }' requirements.txt)"
-
-# check out source code
-rm -rf psycopg2
-git config --global advice.detachedHead false
-git clone --branch "${PSYCOPG2_TAG}" --depth 1 https://github.com/psycopg/psycopg2.git
+apk add --no-cache gcc musl-dev postgresql-dev
 
 # build
-cd psycopg2 || exit
-python setup.py bdist_wheel
+pip wheel --requirement requirements.txt
